4f595ee4af6d88d9f4a0734b2e2bc24f1acbc1b8
Adam Godfrey <ag2795@bath.ac.uk>
2025-12-04 17:20:54 +0000

    fix in list of columns

--- model_test_gradient_boosting.py
+++ model_test_gradient_boosting.py
@@ -8,8 +8,23 @@ from load_data import load_data
 
 THRESHOLD = 0.5
 
-FEATURE_COLS = [...]  # same list
-
+FEATURE_COLS = [
+    'ff_biotac_1','ff_biotac_2','ff_biotac_3','ff_biotac_4','ff_biotac_5',
+    'ff_biotac_6','ff_biotac_7','ff_biotac_8','ff_biotac_9','ff_biotac_10',
+    'ff_biotac_11','ff_biotac_12','ff_biotac_13','ff_biotac_14','ff_biotac_15',
+    'ff_biotac_16','ff_biotac_17','ff_biotac_18','ff_biotac_19','ff_biotac_20',
+    'ff_biotac_21','ff_biotac_22','ff_biotac_23','ff_biotac_24',
+    'mf_biotac_1','mf_biotac_2','mf_biotac_3','mf_biotac_4','mf_biotac_5',
+    'mf_biotac_6','mf_biotac_7','mf_biotac_8','mf_biotac_9','mf_biotac_10',
+    'mf_biotac_11','mf_biotac_12','mf_biotac_13','mf_biotac_14','mf_biotac_15',
+    'mf_biotac_16','mf_biotac_17','mf_biotac_18','mf_biotac_19','mf_biotac_20',
+    'mf_biotac_21','mf_biotac_22','mf_biotac_23','mf_biotac_24',
+    'th_biotac_1','th_biotac_2','th_biotac_3','th_biotac_4','th_biotac_5',
+    'th_biotac_6','th_biotac_7','th_biotac_8','th_biotac_9','th_biotac_10',
+    'th_biotac_11','th_biotac_12','th_biotac_13','th_biotac_14','th_biotac_15',
+    'th_biotac_16','th_biotac_17','th_biotac_18','th_biotac_19','th_biotac_20',
+    'th_biotac_21','th_biotac_22','th_biotac_23','th_biotac_24'
+]
 
 def _load_xy():
     df_tr = load_data("bts3v2_palm_down")
